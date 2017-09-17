9ec81a476609bebfad298ef779dbe2b345ba277b
Aaron <xiepeng.cn@gmail.com>
2017-09-17 23:57:06 +0800

    init

---
+++ app/index.js
@@ -0,0 +1,83 @@
+import React, { Component } from 'react'
+import ReactDOM from 'react-dom'
+import PropTypes from 'prop-types'
+import { createStore } from 'redux'
+import { Provider, connect } from 'react-redux'
+
+import { Router, Route, hashHistory } from 'react-router'
+
+class Counter extends Component {
+    render() {
+        const { value, onBtnClick } = this.props
+        return (
+            <div>
+                <span>{value}</span>
+                <button onClick={(evt) => { onBtnClick(evt.target.textContent) }}>Increase</button>
+                <button onClick={(evt) => { onBtnClick(evt.target.textContent) }}>Decrease</button>
+            </div>
+        )
+    }
+}
+
+Counter.PropTypes = {
+    value: PropTypes.number.isRequired,
+    onBtnClick: PropTypes.func.isRequired
+}
+
+
+function counter(state = { count: 10 }, action) {
+    const count = state.count
+    switch (action.type) {
+        case 'numchange':
+            return action.payload == 'increase' ? { count: count + 1 } : { count: count - 1 }
+        default:
+            return state
+    }
+}
+
+const store = createStore(counter)
+
+const getNumAction = (text) => {
+    return { type: 'numchange', payload: text == 'Increase' ? 'increase' : 'decrease' }
+}
+
+const App = connect(
+    (state) => {
+        return {
+            value: state.count
+        }
+    },
+    (dispatch) => {
+        return {
+            onBtnClick: (text) => dispatch(getNumAction(text))
+        }
+    }
+)(Counter)
+
+const Bpp = connect(
+    (state) => {
+        return {
+            value: state.count
+        }
+    },
+    (dispatch) => {
+        return {
+            onBtnClick: (text) => dispatch(getNumAction(text))
+        }
+    }
+)((self) => {
+    
+    const { value, onBtnClick } = self
+    return (<div>Bpp value:{value}</div>)
+})
+
+ReactDOM.render(
+
+    <Provider store={store}>
+        <Router history={hashHistory}>
+            <Route path="/a" component={App} />
+            <Route path="/b" component={Bpp} />
+        </Router>
+    </Provider>,
+    document.getElementById('root')
+)
---
+++ webpack.config.js
@@ -0,0 +1,53 @@
+
+const webpack = require('webpack');
+const htmlWebpackPlugin = require('html-webpack-plugin');
+const extractTextPlugin = require('extract-text-webpack-plugin');
+
+module.exports = {
+    entry: __dirname + "/app/index.js", 
+    output: {
+        path: __dirname + "/build",
+        filename: "bundle-[hash].js"
+    },
+    devtool: 'sourcemap',
+    devServer: {
+        contentBase: "./public", //本地服务器所加载的页面所在的目录
+        historyApiFallback: true, //不跳转
+        inline: true,
+        hot: true
+    },
+    module: {
+        rules: [{
+            test: /(\.jsx|\.js)$/,
+            use: {
+                loader: "babel-loader"
+            },
+            exclude: /node_modules/
+        }, {
+            test: /\.css$/,
+            use: extractTextPlugin.extract({
+                fallback: "style-loader",
+                use: [{
+                    loader: "css-loader",
+                    options: {
+                        modules: true
+                    }
+                }, {
+                    loader: "postcss-loader"
+                }],
+            })
+        }
+
+        ]
+    },
+    plugins: [
+        new webpack.BannerPlugin('版权所有，翻版必究'),
+        new htmlWebpackPlugin({
+            template: __dirname + "/app/index.tpl.html" //new 一个这个插件的实例，并传入相关的参数
+        }),
+        new webpack.optimize.OccurrenceOrderPlugin(),
+        // new webpack.optimize.UglifyJsPlugin(),
+        new extractTextPlugin("style.css")
+
+    ],
+}

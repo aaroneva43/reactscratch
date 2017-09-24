1af092e0198d990c29a5040f1784d5bf47a8449a
pengxie <pengxie@fortinet.com>
2017-09-24 11:46:39 +0800

    saga init

--- app/containers/Posts.js
+++ app/containers/Posts.js
@@ -6,17 +6,19 @@ import { Provider, connect } from 'react-redux'
 
 import PostList from '../components/PostList'
 export default class Posts extends Comment {
-    static PropTypes = {
-        posts : PropTypes.array.isRequired,
-        isFetching: PropTypes.bool.isRequired
+    static propTypes = {
+        posts: PropTypes.array.isRequired,
+        isFetching: PropTypes.bool.isRequired,
+        dispatch: PropTypes.func.isRequired
     }
 
-    render () {
-        const {posts, isFetching} = this.props
+    render() {
+        const { posts, isFetching } = this.props
 
         return (
 
             <div>
+                <button onClick={this.props.dispatch({type: 'REQUEST', payload: ''})}>load posts</button>
                 <a hidden={!isFetching}>loading...</a>
                 <PostList posts={posts}></PostList>
             </div>

f39a05b876c406c3e2d103ac827fed4a11d627cd
aaron <aaroneva43@163.com>
2023-07-19 09:08:39 -0700

    upgrade boilerplate & ts support

--- webpack.config.js
+++ webpack.config.js
@@ -1,70 +1,76 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
-const path = require('path')
-const webpack = require('webpack');
-const htmlWebpackPlugin = require('html-webpack-plugin');
-const extractTextPlugin = require('extract-text-webpack-plugin');
-
-module.exports = {
-    entry: {
-        app: [
-            'webpack-dev-server/client?http://localhost:8080',
-            // 为 webpack-dev-server 的环境打包代码
-            // 然后连接到指定服务器域名与端口，可以换成本机ip
-
-            'webpack/hot/only-dev-server',
-            // 为热替换(HMR)打包好代码
-            // only- 意味着只有成功更新运行代码才会执行热替换(HMR)
-            './app/index.js']
-    },
+module.exports = (env) => {
+  return {
+    mode: env.production ? "production" : "development",
+    entry: "./src/index.tsx",
+    devtool: env.production ? false : "inline-source-map",
     output: {
-        path: '/',
-        filename: "bundle.js",
-        publicPath: '/'
-    },
-    devtool: 'inline-source-map',
-
-    devServer: {
-        contentBase: './dist',
-        historyApiFallback: true,
-        hot: true
-    },
-    resolve: {
-        extensions: [".jsx", ".json", ".js"]
+      filename: "main.js",
+      path: path.resolve(__dirname, env.production ? "build" : "build_dev"),
     },
     module: {
-        rules: [{
-            test: /(\.jsx|\.js)$/,
-            use: [{
-                loader: "react-hot-loader/webpack"
-            }, {
-                loader: "babel-loader"
-            }],
-            exclude: /node_modules/
-        }, {
-            test: /\.css$/,
-            use: extractTextPlugin.extract({
-                fallback: "style-loader",
-                use: [{
-                    loader: "css-loader",
-                    options: {
-                        modules: true
-                    }
-                }, {
-                    loader: "postcss-loader"
-                }],
-            })
-        }
-
-        ]
+      rules: [
+        {
+          test: /\.(js)$/,
+          exclude: /node_modules/,
+          use: ["babel-loader"],
+        },
+        {
+          test: /\.ts(x?)$/i,
+          loader: "ts-loader",
+          exclude: "/node_modules/",
+        },
+        {
+          test: /\.(png|svg|jpg|jpeg|gif)$/i,
+          type: "asset/resource",
+          generator: {
+            filename: "assets/images/[name].[contenthash][ext]",
+          },
+        },
+        {
+          test: /\.(woff|woff2|eot|ttf|otf)$/i,
+          type: "asset/resource",
+          generator: {
+            filename: "assets/fonts/[name].[contenthash][ext]",
+          },
+        },
+        ...(env.production
+          ? [
+              {
+                test: /\.css$/i,
+                use: [
+                  MiniCssExtractPlugin.loader,
+                  "css-loader",
+                  "postcss-loader",
+                ],
+              },
+            ]
+          : [
+              {
+                test: /\.css$/i,
+                use: ["style-loader", "css-loader"],
+              },
+            ]),
+      ],
+    },
+    // pass all js files through Babel
+    resolve: {
+      extensions: [".js", ".ts", ".tsx"],
     },
     plugins: [
-        new htmlWebpackPlugin({
-            template: __dirname + "/app/index.tpl.html"
-        }),
-        // new webpack.optimize.OccurrenceOrderPlugin(),
-        // new webpack.optimize.UglifyJsPlugin(),
-        new extractTextPlugin("style.css"),
-        new webpack.HotModuleReplacementPlugin()
-
+      new HtmlWebpackPlugin({
+        template: path.join(__dirname, "public", "index.html"),
+      }),
+      ...(env.production ? [new MiniCssExtractPlugin()] : []),
     ],
-}
+    devServer: {
+      static: {
+        directory: path.join(__dirname, "build_dev"),
+      },
+      port: 3002,
+    },
+  };
+};
